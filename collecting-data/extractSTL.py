e5389ab5f00bea4064150a0fba5bfe01e95abf70
muen1019 <113113113aaasssddd@gmail.com>
2026-01-10 18:54:47 +0800

    Add error logging to STL extraction process and remove obsolete STL files

--- collecting-data/extractSTL.py
+++ collecting-data/extractSTL.py
@@ -1,19 +1,23 @@
 import zipfile
 import os
 import shutil
-baseDir = r"P:\ytp_oasis\collecting-data\downloaded"
-outputDir = r"P:\ytp_oasis\collecting-data\outputTest"
+baseDir = r"/tmp2/b14902031/ytp_oasis/collecting-data/scanfiles"
+outputDir = r"/tmp2/b14902031/ytp_oasis/collecting-data/stlFiles"
 zipNameBase = r"OrthoCAD_Export_"
 stlNameSuffix = r"_shell_lockedocclusion_ul.stl"
 
-for uuid in os.listdir(baseDir)[:3]:
+for uuid in os.listdir(baseDir):
     print("UUID", uuid)
-    for zipName in os.listdir(f"{baseDir}\\{uuid}"):
+    for zipName in os.listdir(f"{baseDir}/{uuid}"):
         exportId = zipName[zipName.rfind('_') + 1:-4]
-        exportFilePath = f"{baseDir}\\{uuid}\\{zipNameBase}{exportId}.zip"
+        exportFilePath = f"{baseDir}/{uuid}/{zipNameBase}{exportId}.zip"
         stlFileName = f"{exportId}{stlNameSuffix}"
-        outputFilePath = f"{outputDir}\\{uuid}_{exportId}.stl"
-        with zipfile.ZipFile(exportFilePath, "r") as zipFile:
-            zipFile.extract(stlFileName)
-        shutil.move(stlFileName, outputFilePath)
-        print(f"Extracted completed to {outputFilePath}")
+        outputFilePath = f"{outputDir}/{uuid}_{exportId}.stl"
+        try:
+            with zipfile.ZipFile(exportFilePath, "r") as zipFile:
+                zipFile.extract(stlFileName)
+            shutil.move(stlFileName, outputFilePath)
+            print(f"Extracted completed to {outputFilePath}")
+        except Exception as e:
+            with open("error.txt", "a", encoding = "utf-8") as file:
+                file.write(f"{e}\n")
